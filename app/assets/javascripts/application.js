08f2dc17e02281eec151bdd53b899e05a66befe0
庄司輝拓 <kiseki@shoujikiseki-no-MacBook-Pro.local>
2017-09-11 13:23:44 +0900

    return

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require turbolinks
 //= require jquery/dist/jquery.js
 //= require bootstrap/dist/js/bootstrap.js
 //= require_tree .
