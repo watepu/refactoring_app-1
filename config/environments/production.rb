cefbb514b823583d44d1cb146f220cd4e1e5c443
庄司輝拓 <kiseki@shoujikiseki-no-MacBook-Pro.local>
2017-09-12 21:07:06 +0900

    production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'cryptic-river-46760.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'secret-plains-11212.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
