f6d79c900398f50f7388acedbf01f11de335f731
庄司輝拓 <kiseki@shoujikiseki-no-MacBook-Pro.local>
2017-09-13 15:59:14 +0900

    actionmailer

--- app/mailers/picture_mailer.rb
+++ app/mailers/picture_mailer.rb
@@ -2,7 +2,7 @@ class PictureMailer < ApplicationMailer
 
   def picture_mail(picture)
     @picture = picture
-    mail to: "#{@picture.user.email}",
+    mail to: @picture.user.email,
     subject: "投稿確認メール"
   end
 end
