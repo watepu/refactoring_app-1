031fdd77456f3770791d3b15212d901df4dc2758
watepu <40508321+watepu@users.noreply.github.com>
2018-11-08 20:32:54 +0900

    model/user.rbのリファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 30}
-  validates :email, presence: true, length: { maximum: 255},
-              format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  before_save { email.downcase! }
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
-
   has_many :pictures, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorites_pictures, through: :favorites, source: :picture
+
+  validates :name, presence: true, length: { maximum: 30}
+  validates :email, presence: true, length: { maximum: 255},format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  has_secure_password
+  before_save { email.downcase! }
+
 end
