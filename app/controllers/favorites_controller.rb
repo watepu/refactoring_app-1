cc0af8aed11fc27dcbbb491e0821b3a02125873e
watepu <40508321+watepu@users.noreply.github.com>
2018-11-08 19:58:44 +0900

    favorite_controllerのリファクタリング

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,5 @@
 class FavoritesController < ApplicationController
-  before_action :check_user, only: [:index]
+  before_action :check_user, only: [:index,]
 
   def index
     @favorites = current_user.favorites_pictures
@@ -7,20 +7,17 @@ class FavoritesController < ApplicationController
 
   def create
     set_picture
-    favorite = current_user.favorites.create(picture_id: params[:picture_id])
-      redirect_to pictures_path
-    # redirect_to picture_path(params[:picture_id]), notice: "#{favorite.picture.user.name}さんのブログをお気に入り登録しました"
+    current_user.favorites.create(picture_id: params[:picture_id])
+    redirect_to pictures_path
   end
 
   def destroy
-    # set_picture
-    favorite = current_user.favorites.find_by(picture_id: params[:id]).destroy
+    current_user.favorites.find_by(picture_id: params[:id]).destroy
     if params[:content]
       redirect_to favorites_path
     else
       redirect_to pictures_path
     end
-    # redirect_to picture_path(params[:picture_id]), notice: "#{favorite.picture.user.name}さんのブログをお気に入り解除しました"
   end
 
   private
