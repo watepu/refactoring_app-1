b0d6f1d98f5864d9306e2692ccf726c1f4d14b01
watepu <40508321+watepu@users.noreply.github.com>
2018-11-08 21:03:57 +0900

    application_helper.rb修正、各html.erbファイルにヘルパー定義

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,10 @@ module ApplicationHelper
   def good_on_off_button_helper(picture_id, user_id)
     if Favorite.find_by(picture_id: picture_id, user_id: user_id) && controller.controller_name == "favorites"
       link_to 'いいねを取り消す', favorite_path(picture_id, content: 'favo'), method: :delete, class: 'btn btn-danger'
-    elsif Favorite.find_by(picture_id: picture_id, user_id: user_id)) && controller.controller_name == "pictures"
+    elsif Favorite.find_by(picture_id: picture_id, user_id: user_id) && controller.controller_name == "pictures"
       link_to 'いいねを取り消す', favorite_path(picture_id), method: :delete, class: 'btn btn-danger'
     else
-      link_to 'いいね', favorite_path(picture_id),method: :post, class: 'btn btn-primary'
+      link_to 'いいね!', favorites_path(picture_id: picture_id),method: :post, class: 'btn btn-primary'
     end
   end
 end
