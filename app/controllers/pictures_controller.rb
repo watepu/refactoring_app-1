60fbb63eec20e35192252d7088170205f158c134
watepu <40508321+watepu@users.noreply.github.com>
2018-11-08 20:09:25 +0900

    pictures_controllerのリファクタリング

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,10 +1,6 @@
 class PicturesController < ApplicationController
-  before_action :check_user, only: [:index, :new, :show, :edit, :destroy]
-  before_action :set_picture, only: [:show, :edit, :update, :destroy]
-
-  def top
-
-  end
+  before_action :check_user, only: [:index, :new, :show, :edit, :destroy,]
+  before_action :set_picture, only: [:show, :edit, :update, :destroy,]
 
   def index
     @pictures = Picture.all
@@ -25,42 +21,27 @@ class PicturesController < ApplicationController
   def create
     @picture = Picture.new(picture_params)
     @picture.user_id = current_user.id
-    respond_to do |format|
 
       if @picture.save
         PictureMailer.picture_mail(@picture).deliver
-        format.html { redirect_to @picture, notice: 'Picture was successfully created.' }
-        format.json { render :show, status: :created, location: @picture }
+        redirect_to @picture, notice: 'Picture was successfully created.'
       else
-        format.html { render :new }
-        format.json { render json: @picture.errors, status: :unprocessable_entity }
+        render :new
       end
-    end
-  end
-
-  def confirm
-    @picture = Picture.new(picture_params)
-    render :new if @picture.invalid?
   end
 
   def update
-    respond_to do |format|
+
       if @picture.update(picture_params)
-        format.html { redirect_to @picture, notice: 'Picture was successfully updated.' }
-        format.json { render :show, status: :ok, location: @picture }
+        redirect_to @picture, notice: 'Picture was successfully updated.'
       else
-        format.html { render :edit }
-        format.json { render json: @picture.errors, status: :unprocessable_entity }
+        render :edit
       end
-    end
   end
 
   def destroy
     @picture.destroy
-    respond_to do |format|
-      format.html { redirect_to user_path(current_user.id), notice: 'Picture was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+      redirect_to user_path(current_user.id), notice: 'Picture was successfully destroyed.'
   end
 
   private
